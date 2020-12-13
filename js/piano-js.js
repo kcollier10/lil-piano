a725aac247da8ac5f8b2a524596a80f85d0726fd
Kristen Collier <kristencollier10@gmail.com>
2020-12-13 16:23:15 -0600

    added keypress events and display key for user

--- js/piano-js.js
+++ js/piano-js.js
@@ -1,4 +1,6 @@
 const keys = document.querySelectorAll(".key");
+const whiteKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ','];
+const blackKeys = ['s', 'd', 'g', 'h', 'j'];
 
 // data note and id must be matching for this to work
 // check your typos before going down a rabbit hole *upside-down face*
@@ -14,4 +16,22 @@ function play (key) {
 
 keys.forEach(key => {
     key.addEventListener('click', () => play(key))
+})
+
+
+// keypress events
+const whiteKeysSelector = document.querySelectorAll('.key.white');
+const blackKeysSelector = document.querySelectorAll('.key.black');
+
+
+
+document.addEventListener('keydown', e => {
+    const key = e.key;
+    const whiteKeyArr = whiteKeys.indexOf(key);
+    const blackKeyArr = blackKeys.indexOf(key);
+
+    if (whiteKeyArr > -1) play(whiteKeysSelector[whiteKeyArr]);
+    // get all of our white keys and get it for the index of that white key arr
+    // finding index of key we pressed and then corresponding to index of array
+    if (blackKeyArr > -1) play(blackKeysSelector[blackKeyArr]);
 })
