a83a538a854818f4ee778b512839d1e701753055
Kristen Collier <kristencollier10@gmail.com>
2020-12-13 15:52:22 -0600

    removed color after click

--- js/piano-js.js
+++ js/piano-js.js
@@ -1,11 +1,15 @@
 const keys = document.querySelectorAll(".key");
 
+// data note and id must be matching for this to work
+// check your typos before going down a rabbit hole *upside-down face*
 function play (key) {
     const sound = document.getElementById(key.dataset.note)
     sound.currentTime = 0;
     sound.play();
     key.classList.add('active');
-    // need to remove the color after
+    sound.addEventListener('ended', () => {
+        key.classList.remove('active');
+        })
 }
 
 keys.forEach(key => {
