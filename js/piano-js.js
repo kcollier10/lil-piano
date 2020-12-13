9084def8fbddbd2997bd004b18a50921b28e5017
Kristen Collier <kristencollier10@gmail.com>
2020-12-13 15:41:19 -0600

    added indicator when sounds are being played

--- js/piano-js.js
+++ js/piano-js.js
@@ -2,7 +2,10 @@ const keys = document.querySelectorAll(".key");
 
 function play (key) {
     const sound = document.getElementById(key.dataset.note)
+    sound.currentTime = 0;
     sound.play();
+    key.classList.add('active');
+    // need to remove the color after
 }
 
 keys.forEach(key => {
